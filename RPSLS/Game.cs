c65f6506b3d06e05549aebbb37262811a348c70a
Ryan Smith <ryan_smith2@hotmail.com>
2020-03-19 15:23:50 -0500

    Added additional write lines to the winner of the round and game showing who wins. Added an additional if statement to determine the winner and displaying the winner.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -18,14 +18,22 @@ namespace RPSLS
         //constructor (Spawner)
         public Game()
         {
-
+            WinningScore = 3;
         }
 
         //Member Methods (Can Do)
         public void RunGame()
         {
             Instructions();
-            Player1.playerChoice();
+            TypeOfPlayer();
+            
+
+            while (DetermineWinner())
+            {
+                Player1.playerChoice();
+                Player2.playerChoice();
+                ComparingGestures();
+            }
         }
 
         public void ComparingGestures() //this is considered a round
@@ -35,46 +43,57 @@ namespace RPSLS
             if (Player1.choice == "Rock" && Player2.choice == "Scissors")
             {
                 Player1Score++;
+                Console.WriteLine("Player one wins the round");
             }
             else if (Player1.choice == "Rock" && Player2.choice == "Lizard")
             {
                 Player1Score++;
+                Console.WriteLine("Player one wins the round");
             }
             else if (Player1.choice == "Rock" && Player2.choice == "Paper")
             {
                 Player2Score++;
+                Console.WriteLine("Player 2 wins the round");
             }
             else if (Player1.choice == "Rock" && Player2.choice == "Spock")
             {
                 Player2Score++;
+                Console.WriteLine("Player 2 Wins the round");
             }
             else if (Player1.choice == "Paper" && Player2.choice == "Scissors")
             {
                 Player2Score++;
+                Console.WriteLine("Player 2 wins the round");
             }
             else if (Player1.choice == "Paper" && Player2.choice == "Spock")
             {
                 Player1Score++;
+                Console.WriteLine("Player 1 wins the round");
             }
             else if (Player1.choice == "Paper" && Player2.choice == "Lizard")
             {
                 Player2Score++;
+                Console.WriteLine("Player 2 wins the round");
             }
             else if (Player1.choice == "Scissors" && Player2.choice == "Lizard")
             {
                 Player1Score++;
+                Console.WriteLine("Player 1 wins the round");
             }
             else if (Player1.choice == "Lizard" && Player2.choice == "Spock")
             {
                 Player1Score++;
+                Console.WriteLine("Player 1 wins the round");
             }
             else if (Player1.choice == "Spock" && Player2.choice == "Scissors")
             {
                 Player1Score++;
+                Console.WriteLine("Player 1 wins the round");
             }
             else
             {
                 Console.WriteLine("This is a tie!");
+                Console.ReadLine();
             }
            
 
@@ -83,8 +102,27 @@ namespace RPSLS
 
         }
 
-        public void DetermineWinner()
+        public bool DetermineWinner()
         {
+            if (Player1Score == WinningScore) 
+            {
+                Console.WriteLine("Game is Over Player 1 Wins the game");
+                Console.ReadLine();
+
+                return false;
+            }
+            else if (Player2Score == WinningScore)
+            {
+                Console.WriteLine("Game is OVer Player 2 Wins the game");
+                Console.ReadLine();
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
 
         }
 
@@ -93,50 +131,28 @@ namespace RPSLS
             //USER OUT PUT
             //user input
             //this method second
-           // int choice;
-           // Console.WriteLine("Enter 1 for human vs ComputerAI or enter 2 for Human vs Human");
-            //choice = int.Parse(Console.ReadLine());
-           // if (choice == 1)
-            //{
-               // Player1 = new Human();
-               // Player2 = new ComputerAI();
-           // }
-           // else
-            //{
-               // Player1 = new Human();
-               // Player2 = new Human();
-           // }
-
-            int choice1; //created int of choice1
-            int choice2;
-            Console.WriteLine("Enter 1 for human and enter 2 for ComputerAI"); 
-            choice1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter 1 for human and enter 2 for ComputerAI");
-            choice2 = int.Parse(Console.ReadLine()); // converting string to a int
-
-            if (choice1 == 1)   // created an if statement where if choice 1 is equal to 1 would be a human and choice 2 would be computerAI
+            int choice;
+            Console.WriteLine("Enter 1 for human vs ComputerAI or enter 2 for Human vs Human");
+            choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
             {
                 Player1 = new Human();
-            }
-            else
-            {
-                Player1 = new ComputerAI();
-            }
-            if (choice2 == 1) //same as above comment but for choice 2
-            {
-                Player2 = new Human();
-            }
-            else
-            {
                 Player2 = new ComputerAI();
+                Console.WriteLine("You have chosen Human vs. Computer");
             }
-
+            else
+            {
+                Player1 = new Human();
+                Player2 = new Human();
+                Console.WriteLine("You have chosen Human vs. Human");
+            }
         }
 
 
         public void Instructions()
         {
-            //whenever
+            Console.WriteLine("Rock beats Scissors, Scissors beats Paper, Paper beats Rock, Rock beats Lizard, Lizard beats Spock, Spock beats Scissors, Scissors beats Lizard, Lizard beats Paper, Paper beats Spock, Spock beats Rock");
+            Console.ReadLine();          
         }
      
 
--- RPSLS/Program.cs
+++ RPSLS/Program.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RPSLS
-{
+{       
     class Program
     {
         static void Main(string[] args)
         {
+            Game myGame = new Game();
+            myGame.RunGame();
         }
     }
 }
