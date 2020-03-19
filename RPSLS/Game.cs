cf32c285aafe29515bde3d0bdaae6d0a66400f7b
Ryan Smith <ryan_smith2@hotmail.com>
2020-03-18 20:49:06 -0500

    Added methods to determine a winner and set players choice

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -6,7 +6,139 @@ using System.Threading.Tasks;
 
 namespace RPSLS
 {
-    class Game
+    class Game  //this is where we will bring together the human and additional human and computerAI to deteremine who wins etc. 
     {
+        //Member variables (Has A)
+        public Player Player1;
+        public Player Player2;
+        public int Player1Score;
+        public int Player2Score;
+        public int WinningScore;
+
+        //constructor (Spawner)
+        public Game()
+        {
+
+        }
+
+        //Member Methods (Can Do)
+        public void RunGame()
+        {
+            Instructions();
+            Player1.playerChoice();
+        }
+
+        public void ComparingGestures() //this is considered a round
+        {
+            //after gestures are picked
+            //this method first
+            if (Player1.choice == "Rock" && Player2.choice == "Scissors")
+            {
+                Player1Score++;
+            }
+            else if (Player1.choice == "Rock" && Player2.choice == "Lizard")
+            {
+                Player1Score++;
+            }
+            else if (Player1.choice == "Rock" && Player2.choice == "Paper")
+            {
+                Player2Score++;
+            }
+            else if (Player1.choice == "Rock" && Player2.choice == "Spock")
+            {
+                Player2Score++;
+            }
+            else if (Player1.choice == "Paper" && Player2.choice == "Scissors")
+            {
+                Player2Score++;
+            }
+            else if (Player1.choice == "Paper" && Player2.choice == "Spock")
+            {
+                Player1Score++;
+            }
+            else if (Player1.choice == "Paper" && Player2.choice == "Lizard")
+            {
+                Player2Score++;
+            }
+            else if (Player1.choice == "Scissors" && Player2.choice == "Lizard")
+            {
+                Player1Score++;
+            }
+            else if (Player1.choice == "Lizard" && Player2.choice == "Spock")
+            {
+                Player1Score++;
+            }
+            else if (Player1.choice == "Spock" && Player2.choice == "Scissors")
+            {
+                Player1Score++;
+            }
+            else
+            {
+                Console.WriteLine("This is a tie!");
+            }
+           
+
+
+
+
+        }
+
+        public void DetermineWinner()
+        {
+
+        }
+
+        public void TypeOfPlayer()
+        {
+            //USER OUT PUT
+            //user input
+            //this method second
+           // int choice;
+           // Console.WriteLine("Enter 1 for human vs ComputerAI or enter 2 for Human vs Human");
+            //choice = int.Parse(Console.ReadLine());
+           // if (choice == 1)
+            //{
+               // Player1 = new Human();
+               // Player2 = new ComputerAI();
+           // }
+           // else
+            //{
+               // Player1 = new Human();
+               // Player2 = new Human();
+           // }
+
+            int choice1; //created int of choice1
+            int choice2;
+            Console.WriteLine("Enter 1 for human and enter 2 for ComputerAI"); 
+            choice1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 1 for human and enter 2 for ComputerAI");
+            choice2 = int.Parse(Console.ReadLine()); // converting string to a int
+
+            if (choice1 == 1)   // created an if statement where if choice 1 is equal to 1 would be a human and choice 2 would be computerAI
+            {
+                Player1 = new Human();
+            }
+            else
+            {
+                Player1 = new ComputerAI();
+            }
+            if (choice2 == 1) //same as above comment but for choice 2
+            {
+                Player2 = new Human();
+            }
+            else
+            {
+                Player2 = new ComputerAI();
+            }
+
+        }
+
+
+        public void Instructions()
+        {
+            //whenever
+        }
+     
+
     }
 }
--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -14,11 +14,11 @@ namespace RPSLS
         public List<string> Gestures = new List<string>(); //creating a string as a list called "gestures" which = the newly created object that is stored as "gestures"
         public Player() //this is the consturctor needed to add our gestures that we make. i.e. "rock, paper etc..."
         {
+            Gestures.Add("Rock");
             Gestures.Add("Scissors"); //using our created list "gesture" and adding in our string names
             Gestures.Add("Paper");
-            Gestures.Add("Rock");
-            Gestures.Add("Spock");
             Gestures.Add("Lizard");
+            Gestures.Add("Spock");
         }
 
         public abstract void playerChoice(); // this is where we start the parent method of playerCHoice, its abstract and WILL be used by the inherited classes, Human and Computer AI. 
